1d7196f609f744bae9ace8dcebfa9c0a5247fdbf
Anthony Turrigiano <anthony.turrigiano@gmail.com>
2016-08-01 12:40:57 +0000

    Update config.js file with src paths

--- config/config.js
+++ config/config.js
@@ -1,4 +1,18 @@
 module.exports = {
     host : process.env.IP || '0.0.0.0',
-    port : process.env.PORT || 8080
+    port : process.env.PORT || 8080,
+    path : {
+        root : "./",
+        sass : "./sass",
+        gulpTasks : "./gulpTasks",
+        src : {
+            css: "./public/src/css",
+            fonts: "./public/src/css",
+            icons: "./public/src/icons",
+            img : "./public/src/img",
+            js : "./public/src/js",
+            videos : "./public/src/videos",
+            favicons : "./public/src/img/favicons",
+        }
+    }
 };
